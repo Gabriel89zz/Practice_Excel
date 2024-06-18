d3a098da5a35ea69059a5f47f2486ffd0e6cda9b
Gabriel Torres <elcomparosh97@gmail.com>
2024-06-18 00:37:23 -0600

    .

--- Practice_Excel/Form1.Designer.cs
+++ Practice_Excel/Form1.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            dtgContent = new DataGridView();
+            Brand = new DataGridViewTextBoxColumn();
+            Model = new DataGridViewTextBoxColumn();
+            Motor = new DataGridViewTextBoxColumn();
+            Price = new DataGridViewTextBoxColumn();
+            btnSaveAsExcel = new Button();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dtgContent).BeginInit();
+            SuspendLayout();
+            // 
+            // dtgContent
+            // 
+            dtgContent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtgContent.Columns.AddRange(new DataGridViewColumn[] { Brand, Model, Motor, Price });
+            dtgContent.Location = new Point(62, 59);
+            dtgContent.Name = "dtgContent";
+            dtgContent.Size = new Size(442, 181);
+            dtgContent.TabIndex = 0;
+            // 
+            // Brand
+            // 
+            Brand.HeaderText = "Brand";
+            Brand.Name = "Brand";
+            // 
+            // Model
+            // 
+            Model.HeaderText = "Model";
+            Model.Name = "Model";
+            // 
+            // Motor
+            // 
+            Motor.HeaderText = "Motor";
+            Motor.Name = "Motor";
+            // 
+            // Price
+            // 
+            Price.HeaderText = "Price";
+            Price.Name = "Price";
+            // 
+            // btnSaveAsExcel
+            // 
+            btnSaveAsExcel.Location = new Point(62, 272);
+            btnSaveAsExcel.Name = "btnSaveAsExcel";
+            btnSaveAsExcel.Size = new Size(111, 50);
+            btnSaveAsExcel.TabIndex = 1;
+            btnSaveAsExcel.Text = "Save as Excel File";
+            btnSaveAsExcel.UseVisualStyleBackColor = true;
+            btnSaveAsExcel.Click += btnSaveAsExcel_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft PhagsPa", 16.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(249, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(95, 29);
+            label1.TabIndex = 2;
+            label1.Text = "Car Info";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(590, 383);
+            Controls.Add(label1);
+            Controls.Add(btnSaveAsExcel);
+            Controls.Add(dtgContent);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dtgContent).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private DataGridView dtgContent;
+        private DataGridViewTextBoxColumn Brand;
+        private DataGridViewTextBoxColumn Model;
+        private DataGridViewTextBoxColumn Motor;
+        private DataGridViewTextBoxColumn Price;
+        private Button btnSaveAsExcel;
+        private Label label1;
     }
 }
--- Practice_Excel/Form1.cs
+++ Practice_Excel/Form1.cs
@@ -1,3 +1,6 @@
+using ClosedXML.Excel;
+using System.Windows.Forms;
+
 namespace Practice_Excel
 {
     public partial class Form1 : Form
@@ -5,6 +8,40 @@ namespace Practice_Excel
         public Form1()
         {
             InitializeComponent();
+        }
+
+       
+
+        private void btnSaveAsExcel_Click(object sender, EventArgs e)
+        {
+            var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Hoja1");
+
+         
+            for (int i = 0; i < dtgContent.Columns.Count; i++)
+            {
+                worksheet.Cell(1, i + 1).Value = dtgContent.Columns[i].HeaderText;
+            }
+
+         
+            for (int i = 0; i < dtgContent.Rows.Count; i++)
+            {
+                for (int j = 0; j < dtgContent.Columns.Count; j++)
+                {
+                    worksheet.Cell(i + 2, j + 1).Value = dtgContent.Rows[i].Cells[j].Value?.ToString();
+                }
+            }
+
+         
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel files (*.xlsx)|*.xlsx";
+            saveFileDialog.Title = "Guardar archivo Excel";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                workbook.SaveAs(saveFileDialog.FileName);
+                MessageBox.Show("El archivo se ha guardado correctamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
